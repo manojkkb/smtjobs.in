2e169e266d6b0ccbe2d551a13037f2546e047fc1
Manoj Kushwaha <manoj.kkb@gmail.com>
2026-02-24 18:46:29 +0530

    dev

--- database/seeders/CountriesSeeder.php
+++ database/seeders/CountriesSeeder.php
@@ -10,20 +10,11 @@ class CountriesSeeder extends MasterSeeder
     public function run(): void
     {
         $countries = [
-            ['name' => 'India', 'iso2' => 'IN', 'iso3' => 'IND', 'phone_code' => '+91', 'latitude' => 20.593684, 'longitude' => 78.962880],
+            ['slug' => 'india', 'name' => 'India',  'iso_code' => 'IND','phone_code' => '+91', 'latitude' => 20.593684, 'longitude' => 78.962880],
         ];
 
-        foreach ($countries as &$country) {
-            $country['slug'] = Str::slug($country['name']);
-            $country['is_active'] = true;
-            $country['created_at'] = now();
-            $country['updated_at'] = now();
-        }
-
         Country::upsert(
-            $countries,
-            ['iso2'], // unique key
-            ['name', 'iso3', 'phone_code', 'latitude', 'longitude', 'slug', 'is_active', 'updated_at']
+            $countries
         );
     }
 }
