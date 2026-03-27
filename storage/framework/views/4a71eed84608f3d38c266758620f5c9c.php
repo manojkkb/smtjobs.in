6483cd5ee51e16cc4500f3558d645db9c55632c2
Manoj Kushwaha <manoj.kkb@gmail.com>
2026-03-27 14:51:31 +0530

    dev

--- storage/framework/views/4a71eed84608f3d38c266758620f5c9c.php
+++ storage/framework/views/4a71eed84608f3d38c266758620f5c9c.php
@@ -8,7 +8,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
-        <span class="text-base text-slate-400 font-medium">Search jobs, location, experience...</span>
+        <span class="text-base text-slate-400 font-medium">Search jobs</span>
     </button>
 
     <!-- Desktop Search Form (visible only on desktop) -->
